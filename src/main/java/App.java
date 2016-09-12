280e3bd34440b0d84f7f13fd82d1438294781d0b
Sheena Nickerson <sheenanick@gmail.com>
2016-09-11 17:30:55 -0700

    add files to create a web app that generates a letter based on user input of sender and recipient

---
+++ src/main/java/App.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/hello.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    get("/favorite_photos", (request, response) ->
+    {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+  }
+}
